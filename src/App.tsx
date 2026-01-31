e538304557d8af8665d1e2d75316e0e59e557ec2
dzidziquist <37249283+dzidziquist@users.noreply.github.com>
2026-01-31 08:01:55 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// added HashRouter import here to replace BrowserRouter
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/hooks/use-theme";
 import Index from "./pages/Index";
 import About from "./pages/About";
@@ -21,7 +22,15 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+
+        {/* 
+          CHANGE: Replaced BrowserRouter with HashRouter.
+          - This ensures GitHub Pages user pages do not 404.
+          - All links now automatically become #/ routes (e.g., #/blog, #/about)
+          - No need to modify NavLink or navbar links
+        */}
+        
+      <HashRouter> 
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
@@ -34,7 +43,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
