7aa822ca55b7f93f44e8889fc02c75d200189c8b
dzidziquist <37249283+dzidziquist@users.noreply.github.com>
2026-01-31 08:03:37 -0800

    Update NavLink
    Removed hash prepending for GitHub Pages compatibility.

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -10,12 +10,10 @@ interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
 
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
   ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
-    // Prepend # to make it work with GitHub Pages user pages
-    const hashTo = `#${to.startsWith("/") ? to : "/" + to}`;
     return (
       <RouterNavLink
         ref={ref}
-        to={hashTo}
+        to={to}
         className={({ isActive, isPending }) =>
           cn(className, isActive && activeClassName, isPending && pendingClassName)
         }
